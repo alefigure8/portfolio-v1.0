638071558b6b61e7cc84fbe4ee506accaa404c79
ale_figure8 <gomeznieto@gmail.com>
2022-07-13 20:34:35 -0300

    image testing

--- portfolio/src/component/Banner.jsx
+++ portfolio/src/component/Banner.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import banner from '../img/banner.jpeg'
 
 const Banner = () => {
   return (
-    <div className='mt-40 mb-40 text-white'>Banner</div>
+    <div className='mt-14 mb-6'>
+      <img className='rounded-xl shadow-xl shadow-zinc-900' src={banner} alt="banner" />
+    </div>
   )
 }
 
