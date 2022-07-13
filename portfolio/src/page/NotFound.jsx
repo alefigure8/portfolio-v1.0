60e8888eb68955f93da81c30b2a070f01977a6ac
ale_figure8 <gomeznieto@gmail.com>
2022-07-13 20:35:04 -0300

    componente page 404

---
+++ portfolio/src/page/NotFound.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const NotFound = () => {
+  return (
+
+    <div className='bg-zinc-600 px-10 py-5 rounded-xl shadow-2xl shadow-zinc-900'>
+    <div className='flex items-center justify-center text-zinc-200 flex-col'>
+    <p className='text-6xl'>404</p>
+    <p className='font-bold text-2xl'>Page not found</p>
+    </div>
+    </div>
+
+      )
+}
+
+export default NotFound
