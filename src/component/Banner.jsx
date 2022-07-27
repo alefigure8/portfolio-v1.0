a96863fc3f43cb19bdd886f6bc39da5727102705
ale_figure8 <gomeznieto@gmail.com>
2022-07-27 09:34:03 -0300

    Add Model ThreeJS in Banner

--- src/component/Banner.jsx
+++ src/component/Banner.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import banner from '../img/banner.jpeg'
+import ModelViewer from '../threeJS/ModelViewer'
 
 const Banner = () => {
   return (
-    <div className='mt-14 mb-6'>
-      <img className='rounded-xl shadow-xl shadow-zinc-900' src={banner} alt="banner" />
-    </div>
+    <hero className='h-500'>
+      <ModelViewer />
+    </hero>
   )
 }
 
