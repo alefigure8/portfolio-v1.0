7dd6b06fb88e54eb3d225f6202cd9494a4ad83b0
ale_figure8 <gomeznieto@gmail.com>
2022-07-27 09:32:51 -0300

    Add Model ThreeJS in Main

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -3,7 +3,6 @@ import Header from '../component/Header'
 import Footer from '../component/Footer'
 import Banner from '../component/Banner'
 import useMode from '../hooks/useMode'
-import ModelViewer from '../threeJS/ModelViewer'
 
 const Main = () => {
   const {mode} = useMode()
@@ -13,7 +12,7 @@ const Main = () => {
       <div className={`transition-all min-h-screen ${mode ? 'body-dark' : 'body-light'}`}>
         <main className='container mx-auto p-8 md:p-5 md:flex md:justify-center'>
           <div className="main-w">
-            <ModelViewer/>
+            <Banner/>
             <Outlet/>
           </div>
         </main>
