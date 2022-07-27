8709d651875229699c6dafd0cb7fab850b847652
ale_figure8 <gomeznieto@gmail.com>
2022-07-27 09:33:32 -0300

    Add margin bottom

--- src/component/Button.jsx
+++ src/component/Button.jsx
@@ -4,7 +4,7 @@ import useMode from '../hooks/useMode'
 const Button = ({children}) => {
   const {mode} = useMode()
   return (
-    <div className='flex justify-center mt-5'>
+    <div className='flex justify-center mt-6'>
       <div className={`rounded-lg py-2 px-4 font-medium hover:bg-teal-500 cursor-pointer transition-all ${mode ? 'text-zinc-800 bg-teal-300 ' : 'text-white bg-teal-700 '}`}>{children}<i className="fa-solid fa-chevron-right ml-5 text-xs"></i></div>
     </div>
   )
