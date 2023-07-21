54ad6baff46da0dbaa52a97cc8114272fd495891
Ale Gomez Nieto <alejandro.gomez@alumnos.frgp.utn.edu.ar>
2023-07-21 16:42:44 -0300

    Corrijo textos

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,6 +1,6 @@
 import {Link} from 'react-router-dom'
 import ButtonPrimary from '../component/Button'
-import profile from '../img/profile_4.png'
+import profile from '../img/perfil_utn.jpg'
 import useProjects from '../hooks/useProjects'
 import Spinner from '../component/Spinner';
 import useMode from '../hooks/useMode'
@@ -26,18 +26,14 @@ const Home = () => {
         </div>
       </section>
       <section className=' transition-all-2 -mt-1'>
-        <h4 className={mode ? 'sub-title' : 'sub-title-light'}>Work</h4>
+        <h4 className={mode ? 'sub-title' : 'sub-title-light'}>Sobre mí</h4>
         <p className={`text-justify parraph ${mode ? 'text-white' : 'text-zinc-800'}`}>
-          Soy un desarrolador backend viviendo en Argentina, apasionado por desarrollar aplicaciones que me exijan continuar aprendiendo. Trabajo con Nodejs, Express, MySQL, MongoDB y Firebase, utilizando en muchos de los proyectos Jaison Web Token, passport, Auth0, como también GraphQL. En el frontend trabajo con React, NextJS, Styled Components, Material UI, Chakra, Bootstrap, Tailwind, etc. <br /> Actualmente estoy realizando la carrera de Técnico Universitario en Programación en la Universidad Tecnológica Nacional. <br /> Publico contentenido sobre lo que voy aprendiendo en mi <Link to='posts' className='link'>Blog</Link>
+          Soy un desarrolador backend viviendo en Argentina, apasionado por desarrollar aplicaciones que me exijan continuar aprendiendo. Trabajé con diversos lenguajes, ya sea realizando aplicaciones webs con JavaScript y C# (.NET), como aplicaciones de software con C++. <br /> Actualmente estoy realizando la carrera de Técnico Universitario en Programación en la Universidad Tecnológica Nacional, en la que también me desenvuelvo como Ayudante de Catedra en la materia Programación 1. <br /> Publico contentenido sobre lo que voy aprendiendo en mi <Link to='posts' className='link'>Blog</Link>
         </p>
-        <Link to='works'><ButtonPrimary>My Portfolio</ButtonPrimary></Link>
+        <Link to='works'><ButtonPrimary>Mi Portfolio</ButtonPrimary></Link>
       </section>
       <section className=' transition-all-3 mt-4'>
         <h4 className={mode ? 'sub-title' : 'sub-title-light'}>Bio</h4>
-        <div className='flex'>
-          <p className={`font-bold ${mode ? 'text-white' : 'text-zinc-800'}`}>1984</p>
-          <p className={`text-justify parraph-bio ${mode ? 'text-white' : 'text-zinc-800'}`}>Nací­ en Buenos Aires, Argentina.</p>
-        </div>
         <div className='flex'>
           <p className={`font-bold ${mode ? 'text-white' : 'text-zinc-800'}`}>2001</p>
           <p className={`text-justify parraph-bio ${mode ? 'text-white' : 'text-zinc-800'}`}>Comencé a desarrollar sitios webs, realizando la primera versión del portal de noticias Obrapública.com</p>
@@ -48,7 +44,7 @@ const Home = () => {
         </div>
         <div className='flex'>
           <p className={`font-bold ${mode ? 'text-white' : 'text-zinc-800'}`}>2011</p>
-          <p className={`text-justify parraph-bio ${mode ? 'text-white' : 'text-zinc-800'}`}>Trabajé desarrollando páginas web junto al Legislador Rafael Gentili.</p>
+          <p className={`text-justify parraph-bio ${mode ? 'text-white' : 'text-zinc-800'}`}>Trabajé desarrollando páginas web junto al Legislador  de la Ciudad de Buenos Aires Rafael Gentili.</p>
         </div>
         <div className='flex'>
           <p className={`font-bold ${mode ? 'text-white' : 'text-zinc-800'}`}>2014</p>
@@ -56,10 +52,14 @@ const Home = () => {
         </div>
         <div className='flex'>
           <p className={`font-bold ${mode ? 'text-white' : 'text-zinc-800'}`}>2019</p>
-          <p className={`text-justify parraph-bio ${mode ? 'text-white' : 'text-zinc-800'}`}>Retomé la programación, realizando cursos en Udemy y Freecode Camp.</p>
+          <p className={`text-justify parraph-bio ${mode ? 'text-white' : 'text-zinc-800'}`}>Retomé la programación, realizando cursos en Udemy, Freecode Camp y Maxi Programa.</p>
         </div>
         <div className='flex'>
           <p className={`font-bold ${mode ? 'text-white' : 'text-zinc-800'}`}>2022</p>
+          <p className={`text-justify parraph-bio ${mode ? 'text-white' : 'text-zinc-800'}`}>Empecé a trabajar en ConstruAR, portal de noticias sobre la obra pública, como frontend junior, realizando tareas con JavaScript, HTML y CSS.</p>
+        </div>
+        <div className='flex'>
+          <p className={`font-bold ${mode ? 'text-white' : 'text-zinc-800'}`}>2023</p>
           <p className={`text-justify parraph-bio ${mode ? 'text-white' : 'text-zinc-800'}`}>Hoy me encuentro cursando el segundo año de la carrera de Técnico Universitario en Programación en la Universidad Tecnológica Nacional (UTN), con un promedio de 9.6</p>
         </div>
       </section>
@@ -69,7 +69,7 @@ const Home = () => {
          Escuchar Ramones, Elliott Smith y All Them Witches; leer libros de Foster Wallace, Salinger y Philip K. Dick; el cine de Paul Thomas Anderson y Stanley Kubrick; sacar <a href="https://www.flickr.com/photos/stupiditytries/" target="_blank"><span className ='link'>fotografías</span></a> en recitales y aprender todo lo que me llame un poco la atención.</p>
       </section>
       <section className='mb-4 transition-all-5'>
-        <h4 className={mode ? 'sub-title' : 'sub-title-light'}>On the web</h4>
+        <h4 className={mode ? 'sub-title' : 'sub-title-light'}>Redes</h4>
         <div className='flex flex-col items-start'>
           <a href="http://github.com/alefigure8" target="_blank"><button className={`social-link py-3 px-4 rounded-lg font-semibold parraph-social ${mode ? 'text-teal-200  underline-link ' : 'text-teal-700 underline-link-light underline-link-light'}`}><i className="fa-brands fa-github mr-2" alt="Icono de Github"></i>@alefigure8</button></a>
           <a href="https://twitter.com/alegomeznieto" target="_blank"><button className={`social-link py-3 px-4 rounded-lg font-semibold parraph-social ${mode ? 'text-teal-200  underline-link ' : 'text-teal-700 underline-link-light'}`}><i className="fa-brands fa-twitter mr-2" alt="Icono de Twitter"></i>@alegomeznieto</button></a>
@@ -79,7 +79,7 @@ const Home = () => {
         </div>
       </section>
       <section className='transition-all-6'>
-        <h4 className={mode ? 'sub-title' : 'sub-title-light'}>Latest post</h4>
+        <h4 className={mode ? 'sub-title' : 'sub-title-light'}>Últimos post</h4>
         <div className='flex justify-center flex-col md:flex-row mt-6 mb-10'>
           {loading ? <Spinner /> : (projects.length > 0 ? projects.map(project => {
             return (
@@ -92,7 +92,7 @@ const Home = () => {
           }).reverse().slice(0, POSTS) : <p className={`text-center parraph ${mode ? 'text-zinc-400' : 'text-zinc-800'}`}>No hay proyectos para mostrar</p>)
           }
         </div>
-        <Link to='posts'><ButtonPrimary>Older posts</ButtonPrimary></Link>
+        <Link to='posts'><ButtonPrimary>Anteriores posts</ButtonPrimary></Link>
       </section>
     </>
   )
